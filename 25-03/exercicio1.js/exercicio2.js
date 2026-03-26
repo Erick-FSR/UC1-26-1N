03b2c7459945f8243f605406ef5fbf268c975862
Erick-FSR <erick.f.rodrigues0@gmail.com>
2026-03-26 19:29:37 -0300

    Create exercicio2.js

---
+++ 25-03/exercicio1.js/exercicio2.js
@@ -0,0 +1,41 @@
+let number1 = 3;
+let number2 = 4;
+let number3 = 5;
+let number4 = 2;
+let number5 = -1;
+let numeber6 = 234;
+
+//---------RESULTADOS------------
+
+let result = number1 + number2;
+let result1 = (number1 * number3) / number4
+let result2 = (number2 - number3) * number5
+let result3 = numeber6 % number3
+
+console.log(result)
+console.log(result1)
+console.log(result2)
+console.log(result3)
+
+//exercicio2
+
+//Variáveis
+let type1 = 2
+let type2 = 5
+
+let yesOrNo = type1 === type2
+let yesOrNo2 = type1 !== type2
+let yesOrNo3 = type1 > type2
+let yesOrNo4 = type1 < type2
+
+//1° questão
+console.log("O primeiro número é igual ao segundo?", yesOrNo)
+
+//2° questão
+console.log("O primeiro número é diferente do segundo?", yesOrNo2)
+
+//3° questão
+console.log("O primeiro número é maior que o segundo?", yesOrNo3)
+
+//4° questão
+console.log("O primeiro número é menor que o segundo?", yesOrNo4)
